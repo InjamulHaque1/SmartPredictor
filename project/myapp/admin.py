54cff27dcc15e8b0afe296cb59b86ccbd3af1903
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2024-03-28 03:26:29 +0600

    Updated House-Details

--- project/myapp/admin.py
+++ project/myapp/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+
 admin.site.register(UserProfile)
 
-admin.site.register(Color)
+# admin.site.register(Category)
 
-admin.site.register(House)
+# admin.site.register(House)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id","title")
+
+admin.site.register(Category, CategoryAdmin)
+
+class HouseAdmin(admin.ModelAdmin):
+    list_display = ("id","title", "productCategory", "area", "address", "price","status")
+    list_editable = ("status",)
+
+admin.site.register(House, HouseAdmin)
--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
-from . import views
-
 from . import views
 
 urlpatterns = [    
     #admin and basic urls
     path('admin/', admin.site.urls),
+    path("houses/", views.houses, name="houses"),
+    path('detailBody/<int:house_id>/', views.detailBody, name='detailBody'),
     path('', views.home, name='home'),
     path('houses/', views.houses, name='houses'),
     path('prediction/', views.prediction, name='prediction'),
@@ -18,3 +20,5 @@ urlpatterns = [
     path('user_profile/', views.user_profile, name='user_profile'),
     path('houses/', views.houses, name='houses'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
